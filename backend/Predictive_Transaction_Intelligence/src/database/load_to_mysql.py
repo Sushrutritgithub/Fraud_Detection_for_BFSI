53669e7e33660066a87f19e4c976dbe27366367f
Sushrutritgithub <sushrutdeshpande05@gmail.com>
2025-12-25 16:35:27 +0530

    Updated database

--- backend/Predictive_Transaction_Intelligence/src/database/load_to_mysql.py
+++ backend/Predictive_Transaction_Intelligence/src/database/load_to_mysql.py
@@ -7,31 +7,36 @@ def load_csv_to_mysql(csv_path):
     conn = get_mysql_connection()
     cursor = conn.cursor()
 
-    # Create table with correct 11 columns
+    # Create table with the new columns
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS transactions (
             id INT AUTO_INCREMENT PRIMARY KEY,
-            transaction_id VARCHAR(50),
-            customer_id VARCHAR(50),
-            kyc_verified VARCHAR(10),
-            account_age_days INT,
-            transaction_amount DOUBLE,
-            channel VARCHAR(50),
-            timestamp DATETIME,
+            time_step INT,
+            transaction_type VARCHAR(50),
+            amount DOUBLE,
+            sender_id VARCHAR(50),
+            sender_old_balance DOUBLE,
+            sender_new_balance DOUBLE,
+            receiver_id VARCHAR(50),
+            receiver_old_balance DOUBLE,
+            receiver_new_balance DOUBLE,
             is_fraud INT,
-            hour INT,
-            day_of_week VARCHAR(20),
-            is_high_value INT
+            is_flagged_fraud INT,
+            is_high_value INT,
+            sender_balance_diff DOUBLE,
+            receiver_balance_diff DOUBLE,
+            is_receiver_merchant INT
         );
     """)
 
-    # Prepare insert query with EXACT 11 CSV columns
+    # Prepare insert query with the new columns
     insert_query = """
         INSERT INTO transactions
-        (transaction_id, customer_id, kyc_verified, account_age_days,
-         transaction_amount, channel, timestamp, is_fraud, hour,
-         day_of_week, is_high_value)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        (time_step, transaction_type, amount, sender_id, sender_old_balance,
+         sender_new_balance, receiver_id, receiver_old_balance, receiver_new_balance,
+         is_fraud, is_flagged_fraud, is_high_value, sender_balance_diff,
+         receiver_balance_diff, is_receiver_merchant)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
     """
 
     # Insert each row
@@ -42,6 +47,5 @@ def load_csv_to_mysql(csv_path):
     cursor.close()
     conn.close()
 
-
 if __name__ == "__main__":
-    load_csv_to_mysql("data/processed/Fraudulent2_processed.csv")
+    load_csv_to_mysql("../../data/processed/Fraudulent_processed.csv")
